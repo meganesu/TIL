e378d206d209438e2adc48c1090965cbce0fb618
Megan Sullivan <msulliv@thoughtworks.com>
2019-06-11 10:14:36 -0700

    [Megan] Adds first listener to display property changes

--- css/flexbox/flexbox.js
+++ css/flexbox/flexbox.js
@@ -1,3 +1,9 @@
 function updateStyle(elementId, attribute, value) {
-
+  const elementToStyle = document.getElementById(elementId);
+  elementToStyle.style[attribute] = value;
 }
+
+const displayElement = document.getElementById("display");
+displayElement.addEventListener("change", (event) => (
+  updateStyle("container", "display", event.target.value)
+));
