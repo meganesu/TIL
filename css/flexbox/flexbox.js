89ef6a44c170259b2276362988d86dcbdfd8a39a
Megan Sullivan <msulliv@thoughtworks.com>
2019-06-17 17:00:44 -0700

    [Megan] Dynamically create container event listeners

--- css/flexbox/flexbox.js
+++ css/flexbox/flexbox.js
@@ -1,9 +1,60 @@
-function updateStyle(elementId, attribute, value) {
+const attributes = {
+  container: {
+    display: [
+      'flex',
+      'inline-flex',
+    ],
+    'flex-direction': [
+      'row',
+      'row-reverse',
+      'column',
+      'column-reverse',
+    ],
+    'flex-wrap': [
+      'nowrap',
+      'wrap',
+      'wrap-reverse',
+    ],
+    'justify-content': [
+      'flex-start',
+      'flex-end',
+      'center',
+      'space-between',
+      'space-around',
+      'space-evenly',
+    ],
+    'align-items': [
+      'flex-start',
+      'flex-end',
+      'center',
+      'stretch',
+      'baseline',
+    ],
+    'align-content': [
+      'flex-start',
+      'flex-end',
+      'center',
+      'stretch',
+      'space-between',
+      'space-around',
+    ]
+  },
+  item: {
+
+  }
+};
+
+const updateStyle = (elementId, attribute, value) => {
   const elementToStyle = document.getElementById(elementId);
   elementToStyle.style[attribute] = value;
-}
+};
 
-const displayElement = document.getElementById("display");
-displayElement.addEventListener("change", (event) => (
-  updateStyle("container", "display", event.target.value)
-));
+// set up container listeners
+Object.entries(attributes.container)
+  .forEach(([key, value]) => {
+    const selectElement = document.getElementById(key);
+    selectElement.addEventListener("change", event => (
+      updateStyle("container", key, event.target.value)
+    ));
+    console.log(key, value);
+  });
