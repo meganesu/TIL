fb645304b97a7e21aba437681cc4bbc044710274
Megan Sullivan <msulliv@thoughtworks.com>
2019-06-19 10:19:48 -0700

    [Megan] Dynamically create container attribute editor

--- css/flexbox/flexbox.js
+++ css/flexbox/flexbox.js
@@ -44,17 +44,66 @@ const attributes = {
   }
 };
 
+// build HTML elements
+const createAttributesEditor = () => {
+  const containerAttributes = attributes.container;
+
+  const containerEditor = document.getElementById("container-attributes");
+
+  Object.entries(containerAttributes)
+    .forEach(([attribute, values]) => {
+      const element = buildEditorElement(attribute, values);
+
+      containerEditor.appendChild(element);
+    });
+
+  // set up container listeners
+  Object.entries(attributes.container)
+    .forEach(([key, value]) => {
+      const selectElement = document.getElementById(key);
+      selectElement.addEventListener("change", event => (
+        updateStyle("container", key, event.target.value)
+      ));
+      console.log(key, value);
+    });
+
+};
+
+const buildEditorElement = (attribute, values) => {
+  // set up <p>
+  const element = document.createElement('p');
+
+  // set up <label>, append to p
+  const label = document.createElement('label');
+  label.innerText = `${attribute}:`;
+  element.appendChild(label);
+
+  // set up <select>, append to p
+  const select = document.createElement('select');
+  select.id = attribute;
+  element.appendChild(select);
+
+  // create first '--' option in dropdown
+  const emptyOption = document.createElement('option');
+  emptyOption.value = '';
+  emptyOption.innerText = '--';
+  select.appendChild(emptyOption);
+
+  // for each value in values:
+  values.forEach(value => {
+    // set up <option>, append to select
+    const option = document.createElement('option');
+    option.value = value;
+    option.innerText = value;
+    select.appendChild(option);
+  });
+
+  return element;
+};
+
 const updateStyle = (elementId, attribute, value) => {
   const elementToStyle = document.getElementById(elementId);
   elementToStyle.style[attribute] = value;
 };
 
-// set up container listeners
-Object.entries(attributes.container)
-  .forEach(([key, value]) => {
-    const selectElement = document.getElementById(key);
-    selectElement.addEventListener("change", event => (
-      updateStyle("container", key, event.target.value)
-    ));
-    console.log(key, value);
-  });
+document.body.onload = createAttributesEditor;
